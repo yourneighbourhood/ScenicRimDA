1a5adcc186f698291d386c266b8e9e696c0a1349
Your Neighbourhood <yourneighbourhood@users.noreply.github.com>
2020-04-22 22:37:26 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,13 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'mechanize'
+require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
+doc = agent.get("https://srr-prod-icon.saas.t1cloud.com/Common/Common/Terms.aspx")
+form = doc.forms.first
+# Tick the box.
+form.checkbox_with(:name => 'ctl00$ctMain$chkAgree$chk1').check
+# Click the button.
+doc = doc.forms.first.submit(doc.forms.first.button_with(:value => "I Agree"), "Accept-Encoding" => "identity")
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Use the ICON XML scraper.
+scrape_icon_rest_xml("https://srr-prod-icon.saas.t1cloud.com/Pages/XC.Track/SearchApplication.aspx", "d=last28days&o=xml", false, agent)
