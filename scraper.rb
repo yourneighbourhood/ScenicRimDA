34320cec4f8ae56020f56e45e00a35e2d8da6a84
Your Neighbourhood <yourneighbourhood@users.noreply.github.com>
2020-04-28 21:50:16 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,75 @@
+require 'scraperwiki'
 require 'mechanize'
-require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
-agent = Mechanize.new
-doc = agent.get("https://srr-prod-icon.saas.t1cloud.com/Common/Common/Terms.aspx")
-form = doc.forms.first
-# Tick the box.
-form.checkbox_with(:name => 'ctl00$ctMain$chkAgree$chk1').check
-# Click the button.
-doc = doc.forms.first.submit(doc.forms.first.button_with(:value => "I Agree"), "Accept-Encoding" => "identity")
+def clean_whitespace(string)
+  string.gsub("\r", ' ').gsub("\n", ' ').squeeze(" ").strip
+end
 
-# Use the ICON XML scraper.
-scrape_icon_rest_xml("https://srr-prod-icon.saas.t1cloud.com/Pages/XC.Track/SearchApplication.aspx", "d=last28days&o=xml", false, agent)
+def scrape_icon_rest_xml(base_url, query, debug = false, agent = nil)
+  agent = Mechanize.new unless agent
+  page = agent.get("#{base_url}?#{query}")
+
+  # Explicitly interpret as XML
+  page = Nokogiri::XML(page.content)
+
+  raise "Can't find any <Application> elements" unless page.search('Application').length > 0
+
+  page.search('Application').each do |application|
+    council_reference = application.at("ReferenceNumber").inner_text
+
+    unless application.at("Address Line1")
+      puts "Skipping due to lack of address for #{council_reference}"
+      next
+    end
+    application_id = application.at("ApplicationId").inner_text
+    info_url = "#{base_url}?id=#{application_id}"
+
+    address = clean_whitespace(application.at("Address Line1").inner_text)
+    if !application.at('Address Line2').inner_text.empty?
+      address += ", " + clean_whitespace(application.at("Address Line2").inner_text)
+    end
+
+    if application.at("ApplicationDetails")
+      description = application.at("ApplicationDetails")
+    else
+      description = application.at("SubNatureOfApplication")
+    end
+
+    unless description
+      puts "Skipping due to lack of description for #{council_reference}"
+      next
+    end
+
+    record = {
+      "council_reference" => council_reference,
+      "description" => description.inner_text,
+      "date_received" => Date.parse(application.at("LodgementDate").inner_text).to_s,
+      # TODO: There can be multiple addresses per application
+      # We can't just create a new application for each address as we would then have multiple applications
+      # with the same council_reference which isn't currently allowed.
+      "address" => address,
+      "date_scraped" => Date.today.to_s,
+      "info_url" => info_url,
+      # Can't find a specific url for commenting on applications.
+      "comment_url" => info_url,
+    }
+    # DA03NY1 appears to be the event code for putting this application on exhibition
+    # Commenting this out because I don't know whether this can be applied generally to all
+    # councils. It seems likely that the event codes are different in each council
+    #e = application.search("Event EventCode").find{|e| e.inner_text.strip == "DA03NY1"}
+    #if e
+    #  record["on_notice_from"] = Date.parse(e.parent.at("LodgementDate").inner_text).to_s
+    #  record["on_notice_to"] = Date.parse(e.parent.at("DateDue").inner_text).to_s
+    #end
+
+    if debug
+      p record
+    else
+      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        ScraperWiki.save_sqlite(['council_reference'], record)
+      else
+        puts "Skipping already saved record " + record['council_reference']
+      end
+    end
+  end
+end
